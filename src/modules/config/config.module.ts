fee00f3d5f8d6a69cc9ee9056033529003a89b3a
vlad-boroda <enity.w@gmail.com>
2024-05-12 18:02:17 +0300

    fix ConfigModule import-time execution

--- src/modules/config/config.module.ts
+++ src/modules/config/config.module.ts
@@ -6,56 +6,58 @@ import type { DynamicModule, Provider } from '@nestjs/common';
 import type { ConfigModuleOpts } from './config-module-opts';
 
 export class ConfigModule {
-  static async forRoot(opts: ConfigModuleOpts): Promise<DynamicModule> {
+  static forRoot(opts: ConfigModuleOpts): DynamicModule {
     return ConfigModule.register(opts, true);
   }
 
-  private static async register(
+  private static register(
     opts: ConfigModuleOpts,
     global: boolean,
-  ): Promise<DynamicModule> {
-    const monkeeOpts: BuildConfigOptions = {
-      ymlFiles: opts.config.ymlFiles,
-      envFiles: opts.config.envFiles,
-      loadEnvFiles: opts.config.loadEnvFiles === true,
-      throwOnValidationError: false,
-      throwIfYmlNotExist: false,
-      throwIfEnvFileNotExist: false,
-    };
-
-    const { config, jsonSchema, validationErrors } = await buildConfig(
-      opts.configClass,
-      monkeeOpts,
-    );
-
-    if (opts.config.generateDocAndSchema === true) {
-      if (opts.config.configDocPath !== undefined) {
-        await generateConfigDoc(jsonSchema, {
-          writeToFile: opts.config.configDocPath,
-        });
-      }
-
-      if (opts.config.configSchemaPath !== undefined) {
-        await writeFile(
-          opts.config.configSchemaPath,
-          JSON.stringify(jsonSchema),
-          'utf-8',
-        );
-      }
-    }
-
-    if (validationErrors.length > 0) {
-      throw new Error(
-        `Config validation failed: \n${validationErrors
-          .map((e) => e.toString())
-          .join('\n')}`,
-      );
-    }
-
+  ): DynamicModule {
     const providers: Provider[] = [
       {
         provide: opts.configClass,
-        useValue: config,
+        useFactory: async () => {
+          const monkeeOpts: BuildConfigOptions = {
+            ymlFiles: opts.config.ymlFiles,
+            envFiles: opts.config.envFiles,
+            loadEnvFiles: opts.config.loadEnvFiles === true,
+            throwOnValidationError: false,
+            throwIfYmlNotExist: false,
+            throwIfEnvFileNotExist: false,
+          };
+
+          const { config, jsonSchema, validationErrors } = await buildConfig(
+            opts.configClass,
+            monkeeOpts,
+          );
+
+          if (opts.config.generateDocAndSchema === true) {
+            if (opts.config.configDocPath !== undefined) {
+              await generateConfigDoc(jsonSchema, {
+                writeToFile: opts.config.configDocPath,
+              });
+            }
+
+            if (opts.config.configSchemaPath !== undefined) {
+              await writeFile(
+                opts.config.configSchemaPath,
+                JSON.stringify(jsonSchema),
+                'utf-8',
+              );
+            }
+          }
+
+          if (validationErrors.length > 0) {
+            throw new Error(
+              `Config validation failed: \n${validationErrors
+                .map((e) => e.toString())
+                .join('\n')}`,
+            );
+          }
+
+          return config;
+        },
       },
     ];
 
